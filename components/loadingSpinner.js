b5f14c9109b3539578d9f11535bd037856960d7d
Shaun VanWeelden <shaun.vanweelden@scaleapi.com>
2021-12-07 03:49:04 +0700

    Make spinner encourage a page refresh 😬

--- components/loadingSpinner.js
+++ components/loadingSpinner.js
@@ -4,8 +4,13 @@ import { Box, CircularProgress } from "@mui/material";
 
 export default function LoadingSpinner() {
   return (
-    <Box sx={{ display: "flex", justifyContent: "center", margin: "100px" }}>
-      <CircularProgress size={100} />
-    </Box>
+    <>
+      <Box sx={{ display: "flex", justifyContent: "center", margin: "100px" }}>
+        <CircularProgress size={100} />
+      </Box>
+      <Box style={{ textAlign: "center" }}>
+        Stuck? Try a quick refresh to keep things, well, fresh
+      </Box>
+    </>
   );
 }
